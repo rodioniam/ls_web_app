5bb2bf8ba3e8a49084da1949d94b80d57aba8e5d
Rodion Iamshanov <roiamshanov@gmail.com>
2026-03-16 17:07:15 +0100

    add main feature - adding and removing points from cards

--- allocator/urls.py
+++ allocator/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('session/<int:session_id>/', views.index, name='index')
+    path('session/<int:session_id>/', views.index, name='index'),
+    path('card/<int:card_id>/<str:action>/',
+         views.change_points, name='change_points')
 ]
--- allocator/views.py
+++ allocator/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Card, Session
 
 
@@ -6,3 +6,28 @@ def index(request, session_id):
     session = get_object_or_404(Session, id=session_id)
     cards = Card.objects.filter(session=session)
     return render(request, 'allocation.html', {'cards': cards, 'session': session})
+
+
+def change_points(request, card_id, action):
+    card = get_object_or_404(Card, id=card_id)
+    session = card.session
+
+    if request.method == "POST":
+        total_used = sum(
+            c.points for c in Card.objects.filter(session=session))
+
+        if action == 'add':
+            if total_used < session.total_points:
+                card.points += 1
+                card.save()
+            # session.total_points -= 1
+            # session.save()
+
+        if action == 'remove':
+            if card.points > 0:
+                card.points -= 1
+                card.save()
+            # session.total_points += 1
+            # session.save()
+
+    return redirect('index', session_id=session.id)
