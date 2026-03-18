d0cf4f8173e10b83da68f096fa78f945de5bfa46
Rodion Iamshanov <roiamshanov@gmail.com>
2026-03-18 17:34:32 +0100

    added new path for home page

--- allocator/urls.py
+++ allocator/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('session/<int:session_id>/', views.index, name='index'),
     path('card/<int:card_id>/<str:action>/',
          views.change_points, name='change_points')
