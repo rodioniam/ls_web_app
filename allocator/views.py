17089fddc2a8706a83a9152e01195272b6002e51
Rodion Iamshanov <roiamshanov@gmail.com>
2026-03-20 20:49:10 +0100

    added new function - multipliers for base card points, changed all functions to support it

--- allocator/views.py
+++ allocator/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template.loader import render_to_string
 from django.http import HttpResponse
-from .models import Card, Session, Multiplier
+from .models import Card, Session, Multiplier, CardMultiplier
 
 
 def home(request):
@@ -31,8 +31,20 @@ def index(request, session_id):
     card_1 = cards[0]
     card_2 = cards[1]
     multipliers = Multiplier.objects.all()
+    card_1_multipliers = CardMultiplier.objects.filter(
+        parent_card_id=card_1.id)
+    card_2_multipliers = CardMultiplier.objects.filter(
+        parent_card_id=card_2.id)
+    card_1_result = card_1.points
+    card_2_result = card_2.points
+    for m in card_1_multipliers:
+        card_1_result *= m.assigned_mults.value
+    for m in card_2_multipliers:
+        card_2_result += m.assigned_mults.value
+
     total_used = sum(c.points for c in cards)
     points_remaining = session.total_points - total_used
+
     return render(request,
                   'allocation.html',
                   {
@@ -40,7 +52,9 @@ def index(request, session_id):
                       'card_2': card_2,
                       'session': session,
                       'points_remaining': points_remaining,
-                      'multipliers': multipliers
+                      'multipliers': multipliers,
+                      'card_1_result': card_1_result,
+                      'card_2_result': card_2_result
                   }
                   )
 
@@ -68,8 +82,14 @@ def change_points(request, card_id, action):
             c.points for c in Card.objects.filter(session=session))
         points_remaining = session.total_points - total_used
 
+        card_multipliers = CardMultiplier.objects.filter(parent_card=card)
+        card_result = card.points
+
+        for m in card_multipliers:
+            card_result *= m.assigned_mults.value
+
         card_html = render_to_string(
-            'card.html', {'card': card}, request=request)
+            'card.html', {'card': card, 'points': card_result}, request=request)
         counter_html = render_to_string('points_remaining.html', {
                                         'points_remaining': points_remaining}, request=request)
 
@@ -81,10 +101,41 @@ def change_points(request, card_id, action):
 def reset_points(request, session_id):
     session = get_object_or_404(Session, id=session_id)
     cards = Card.objects.filter(session=session)
+    card_multipliers = CardMultiplier.objects.filter(parent_card__in=cards)
 
     if request.method == "POST":
         for card in cards:
             card.points = 0
             card.save()
+        card_multipliers.delete()
 
     return redirect('index', session_id=session.id)
+
+
+def assign_multiplier(request, session_id):
+    card_id = request.POST['card_id']
+    multiplier_id = request.POST['multiplier_id']
+    card = get_object_or_404(Card, id=card_id)
+    multiplier = get_object_or_404(Multiplier, id=multiplier_id)
+
+    if request.POST.get('left-card') or request.POST.get('right-card'):
+        CardMultiplier.objects.create(
+            parent_card=card,
+            assigned_mults=multiplier
+        )
+        card_multipliers = CardMultiplier.objects.filter(parent_card=card)
+        card_result = card.points
+        for m in card_multipliers:
+            card_result *= m.assigned_mults.value
+        return render(request, 'card.html', {'card': card, 'points': card_result})
+    else:
+        CardMultiplier.objects.filter(
+            parent_card=card,
+            assigned_mults=multiplier
+        ).delete()
+
+        card_multipliers = CardMultiplier.objects.filter(parent_card=card)
+        card_result = card.points
+        for m in card_multipliers:
+            card_result *= m.assigned_mults.value
+        return render(request, 'card.html', {'card': card, 'points': card_result})
