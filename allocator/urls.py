22523c9c67fe5b31a3edb2fe9d3d07c8a2f537d0
Rodion Iamshanov <roiamshanov@gmail.com>
2026-03-20 20:47:55 +0100

    added new path for multipliers

--- allocator/urls.py
+++ allocator/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('card/<int:card_id>/<str:action>/',
          views.change_points, name='change_points'),
     path('session/<int:session_id>/reset/',
-         views.reset_points, name='reset_points')
+         views.reset_points, name='reset_points'),
+    path('session/<int:session_id>/multipliers/',
+         views.assign_multiplier, name='assign_multiplier')
 ]
