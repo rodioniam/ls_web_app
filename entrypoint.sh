07c1f1db71d43a24d35ee1755e4e2f0c6d316266
Rodion Iamshanov <roiamshanov@gmail.com>
2026-04-21 22:00:39 +0200

    add Docker configuration with PostgreSQL

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py loaddata allocator/fixtures/multipliers.json
-gunicorn base.wsgi:application --bind 0.0.0.0:8000
+gunicorn base.wsgi:application --bind 0.0.0.0:${PORT:-8000}
